5e28bb4e2bc4d47134aeaf4d808f47aa06195ee0
orhun <orhunparmaksiz@gmail.com>
2020-10-16 02:23:18 +0300

    Add wifi script

---
+++ scripts/scripts/wifi.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+
+# This is a brief summary of connecting to a WPA/WPA2 WiFi network
+# https://linuxcommando.blogspot.com/2013/10/how-to-connect-to-wpawpa2-wifi-network.html
+
+# Find out the wireless device name.
+# iw dev
+
+# Check if the device is up.
+# ip link set interface up
+ip link set wlp3s0 up
+
+# Check connection status.
+# iw interface link
+
+# Scan networks.
+# sudo iw interface scan | less
+
+# Connect to WPA/WPA2 WiFi network.
+# wpa_passphrase SSID >> /etc/wpa_supplicant.conf
+# wpa_supplicant -B -D wext -i interface -c /etc/wpa_supplicant.conf -f /var/log/wpa_supplicant.log -t -d
+wpa_supplicant -B -D wext -i wlp3s0 -c /etc/wpa_supplicant.conf -f /var/log/wpa_supplicant.log -t -d
+# dhcpcd
+dhcpcd
