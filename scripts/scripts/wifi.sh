f90d3d69bb72728faf837fa9a6dc2c8c15649cfb
orhun <orhunparmaksiz@gmail.com>
2020-11-19 11:28:55 +0300

    Update dotfiles

--- scripts/scripts/wifi.sh
+++ scripts/scripts/wifi.sh
@@ -8,7 +8,8 @@
 
 # Check if the device is up.
 # ip link set interface up
-ip link set wlp3s0 up
+ip link set wlan1 up
+#ip link set wlp3s0 up
 
 # Check connection status.
 # iw interface link
@@ -19,6 +20,7 @@ ip link set wlp3s0 up
 # Connect to WPA/WPA2 WiFi network.
 # wpa_passphrase SSID >> /etc/wpa_supplicant.conf
 # wpa_supplicant -B -D wext -i interface -c /etc/wpa_supplicant.conf -f /var/log/wpa_supplicant.log -t -d
-wpa_supplicant -B -D wext -i wlp3s0 -c /etc/wpa_supplicant.conf -f /var/log/wpa_supplicant.log -t -d
+wpa_supplicant -B -D wext -i wlan1 -c /etc/wpa_supplicant.conf -f /var/log/wpa_supplicant.log -t -d
+#wpa_supplicant -B -D wext -i wlp3s0 -c /etc/wpa_supplicant.conf -f /var/log/wpa_supplicant.log -t -d
 # dhcpcd
 dhcpcd
