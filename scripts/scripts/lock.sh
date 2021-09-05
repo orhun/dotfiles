71cb759055549c319bb6bda223999aaf85728aeb
orhun <orhun@archlinux.org>
2021-09-05 12:20:06 +0300

    Update lock.sh

--- scripts/scripts/lock.sh
+++ scripts/scripts/lock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-i3lock-fancy -g -f Source-Code-Pro -t "" -- menyoki -q cap --root png -c fast save 2>/dev/null
+i3lock-fancy -g -t "" -- menyoki -q cap --root png -c fast save 2>/dev/null
