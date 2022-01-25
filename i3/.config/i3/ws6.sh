45ea0a8d25fc3232d7013919e0a42704b646f8ad
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-01-25 20:43:22 +0300

    Update i3 config

--- i3/.config/i3/ws6.sh
+++ i3/.config/i3/ws6.sh
@@ -4,11 +4,10 @@ set -e
 
 if ! i3-msg -t get_workspaces | rg '"num":6'; then
   i3-msg 'workspace 6'
-  i3-msg 'exec kermit -e "bpytop"' && sleep 2
-  i3-msg 'split v; exec kermit -e "journalctl -fex"' && sleep 2
+  i3-msg 'exec alacritty -e bpytop' && sleep 2
+  i3-msg 'split v; exec alacritty -e journalctl -fex' && sleep 2
   i3-msg 'resize shrink up 150'
-  i3-msg 'exec kermit -e "dmesg -w --color=never"' && sleep 2
+  i3-msg 'exec alacritty -e dmesg -w --color=never' && sleep 2
 else
   i3-msg 'workspace 6'
 fi
-
