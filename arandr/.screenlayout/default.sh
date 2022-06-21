2327cb28f7e0a8af125773e14519ce8357123c1c
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-06-21 12:06:16 +0200

    Add arandr config

---
+++ arandr/.screenlayout/default.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off
+"$HOME/.fehbg"
---
+++ arandr/.screenlayout/multimonitor.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+xrandr --output eDP --primary --mode 1920x1080 --pos 3840x1080 --rotate normal --output HDMI-A-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-0 --off
+"$HOME/.fehbg"
