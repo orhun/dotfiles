c7eaa169b4c5447023e085d54056c723586035a4
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-06-24 09:23:52 +0200

    Update arandr script

--- arandr/.screenlayout/multimonitor.sh
+++ arandr/.screenlayout/multimonitor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP --primary --mode 1920x1080 --pos 3840x1080 --rotate normal --output HDMI-A-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-0 --off
+xrandr --output eDP --primary --mode 1920x1080 --pos 1146x2160 --rotate normal --output HDMI-A-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-0 --off
 "$HOME/.fehbg"
