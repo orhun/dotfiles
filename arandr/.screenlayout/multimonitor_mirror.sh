b930be5d277baee45f1d559b8e57b4ddaaafa57b
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-06-27 11:26:43 +0200

    Update arandr config

---
+++ arandr/.screenlayout/multimonitor_mirror.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off
+"$HOME/.fehbg"
