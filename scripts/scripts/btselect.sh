4b930562278571a2502ab39f4d43bb6fe9db0bc6
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-06-28 21:06:06 +0200

    Add Bluetooth selector script

---
+++ scripts/scripts/btselect.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+devices=$(bluetoothctl paired-devices | cut -f 2- -d ' ')
+
+options=()
+while read -r n s; do
+	options+=("$n" "$s")
+done <<<"$devices"
+
+device=$(dialog --clear \
+	--backtitle "BT Select" \
+	--title "Bluetooth" \
+	--menu "Select the device to connect:" \
+	15 40 4 \
+	"${options[@]}" \
+	2>&1 >/dev/tty)
+
+bluetoothctl power on
+bluetoothctl connect "$device"
