c7af5a7f44cd95dd272fe23a0f586920fefaf880
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-09-04 19:23:53 +0200

    Add anti-afk script

---
+++ scripts/scripts/antiafk.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+randomchar() {
+ openssl rand -base64 1 | cut -c1-1
+}
+
+while true; do
+  for i in {1..20}; do
+    xdotool type $(randomchar)
+    sleep 0.3s
+  done
+
+  for i in {1..20}; do
+    xdotool key BackSpace
+    sleep 0.1s
+  done
+done
+
