2babd5b2b94517a58e889ea4798a95afba9bc2db
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-09-04 19:41:44 +0200

    Update files

--- i3/.config/i3/ws6.sh
+++ i3/.config/i3/ws6.sh
@@ -4,10 +4,10 @@ set -e
 
 if ! i3-msg -t get_workspaces | rg '"num":6'; then
   i3-msg 'workspace 6'
-  i3-msg 'exec alacritty -e bpytop' && sleep 2
-  i3-msg 'split v; exec alacritty -e journalctl -fex' && sleep 2
+  i3-msg 'exec alacritty -e bpytop' && sleep 1
+  i3-msg 'split v; exec alacritty -e journalctl -fex' && sleep 1
   i3-msg 'resize shrink up 150'
-  i3-msg 'exec alacritty -e dmesg -w --color=never' && sleep 2
+  i3-msg 'split h; exec alacritty -e dmesg -w --color=never' && sleep 1
 else
   i3-msg 'workspace 6'
 fi
