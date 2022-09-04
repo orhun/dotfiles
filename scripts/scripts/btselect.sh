c07036faf414a74beceb499d45092bc30f672f0e
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-09-04 19:23:41 +0200

    Update BT selector script

--- scripts/scripts/btselect.sh
+++ scripts/scripts/btselect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-devices=$(bluetoothctl paired-devices | cut -f 2- -d ' ')
+devices=$(bluetoothctl devices Paired | cut -f 2- -d ' ')
 
 options=()
 while read -r n s; do
@@ -11,7 +11,7 @@ device=$(dialog --clear \
 	--backtitle "BT Select" \
 	--title "Bluetooth" \
 	--menu "Select the device to connect:" \
-	15 40 4 \
+	15 50 4 \
 	"${options[@]}" \
 	2>&1 >/dev/tty)
 
