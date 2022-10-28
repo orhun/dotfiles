57f2f6a2992a7d9be2844128c02eeb3b41888278
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-10-28 22:04:14 +0300

    docker: Add docker-compose restart alias

--- bash/.bashrc.d/docker_aliases.sh
+++ bash/.bashrc.d/docker_aliases.sh
@@ -2,3 +2,4 @@
 
 alias docker-ls-restart="docker container ls -q | xargs docker container inspect --format '{{ .Name }}: {{.HostConfig.RestartPolicy.Name}}'"
 alias docker-stop-all="docker ps -q | xargs docker stop"
+alias docker-compose-restart="docker compose up --force-recreate --build -d"
