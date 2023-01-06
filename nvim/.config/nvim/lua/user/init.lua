cd60eb985476539f688e5c616ec015841140f774
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-01-06 20:39:14 +0300

    nvim: Update command description

--- nvim/.config/nvim/lua/user/init.lua
+++ nvim/.config/nvim/lua/user/init.lua
@@ -177,7 +177,7 @@ local config = {
       ["<C-d>"] = { "<C-d>zz", desc = "Scroll down" },
       ["<C-u>"] = { "<C-u>zz", desc = "Scroll up" },
       ["<leader>U"] = { ":UndotreeToggle<cr>", desc = "Toggle undo history" },
-      ["<leader>gg"] = { ":Git<cr>", desc = "Open git" },
+      ["<leader>gg"] = { ":Git<cr>", desc = "Open Git" },
       ["<leader>m"] = { ":MarkdownPreview<cr>", desc = "Show markdown preview" },
       ["<leader>z"] = { ":ZenMode<cr>", desc = "Toggle Zen mode" },
     },
