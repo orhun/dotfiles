e1a539820b3f03a62f84915da51cf18adfb7a6e8
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-04-10 16:13:54 +0300

    i3: Use a custom Zellij layout for monitoring workspace

--- i3/.config/i3/ws6.sh
+++ i3/.config/i3/ws6.sh
@@ -4,10 +4,7 @@ set -e
 
 if ! i3-msg -t get_workspaces | rg '"num":6'; then
   i3-msg 'workspace 6'
-  i3-msg 'exec alacritty -e bpytop' && sleep 1
-  i3-msg 'split v; exec alacritty -e journalctl -fex' && sleep 1
-  i3-msg 'resize shrink up 150'
-  i3-msg 'split h; exec alacritty -e dmesg -w --color=never' && sleep 1
+  i3-msg 'exec alacritty -e zellij --layout monitoring'
 else
   i3-msg 'workspace 6'
 fi
