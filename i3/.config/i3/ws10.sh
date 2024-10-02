f6e079b920ef19d2d2f33510d9e658d5ff069533
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-10-02 12:13:13 +0300

    i3: Remove workspace groups

--- i3/.config/i3/ws10.sh
+++ i3/.config/i3/ws10.sh
@@ -3,8 +3,8 @@
 set -e
 
 if ! i3-msg -t get_workspaces | rg '"num":10'; then
-	i3-workspace-groups-client workspace-number 10
+	i3-msg 'workspace 10'
 	i3-msg 'exec alacritty -e tjournal'
 else
-	i3-workspace-groups-client workspace-number 10
+	i3-msg 'workspace 10'
 fi
