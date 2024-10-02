56112f712b4f5041b378824add8c003f35fe2a5c
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-10-02 12:16:12 +0300

    arandr: Update screen layouts

--- arandr/.screenlayout/default.sh
+++ arandr/.screenlayout/default.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-
+#!/bin/sh
 xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off
 "$HOME/.fehbg"
---
+++ arandr/.screenlayout/stream.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off
+"$HOME/.fehbg"
