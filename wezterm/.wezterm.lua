e124ea9301850af651b8afc8c5dc5e782b80bec9
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-10-16 11:08:13 +0300

    wezterm: Update color config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,13 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.color_scheme = "Kolorit"
+config.colors = {
+	background = "#171717",
+	foreground = "#fff",
+}
+config.color_scheme = "Gruvbox dark, pale (base16)"
 config.enable_tab_bar = false
+
 config.font = wezterm.font("Fira Code")
 config.font_size = 15.0
 
