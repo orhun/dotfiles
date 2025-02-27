6f70a5eaf1a21f53ec2f008363fc75360d3d0fc6
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2025-02-28 00:32:06 +0300

    wezterm: Update config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,13 +2,21 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 config.colors = {
-	background = "#171717",
+	background = "#151515",
 	foreground = "#fff",
 }
 config.color_scheme = "Gruvbox dark, pale (base16)"
 config.enable_tab_bar = false
 
-config.font = wezterm.font("Fira Code")
-config.font_size = 15.0
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 25.0
+
+config.keys = {
+	{
+		key = "0",
+		mods = "CTRL",
+		action = wezterm.action.ResetFontSize,
+	},
+}
 
 return config
